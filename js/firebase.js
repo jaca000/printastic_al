edb6de32df83aea9977c8a0c9864668b3f2f1bc2
jaca000 <almanso.jose@gmail.com>
2026-02-05 14:50:51 +0000

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,5 +1,8 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+export const firebaseConfig = {
   apiKey: "AIzaSyC3eYuvhZkhjViDLP2h8_pvhNMN_d4Q2J8",
   authDomain: "printastic-al.firebaseapp.com",
   projectId: "printastic-al",
@@ -8,3 +11,8 @@ const firebaseConfig = {
   appId: "1:894753780425:web:490b24f721c4d655fed318",
   measurementId: "G-BZBCMKQ8S7"
 };
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
