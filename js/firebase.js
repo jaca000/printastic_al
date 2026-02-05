9c13fef511777ddbbe73930b5e3c3ade114a5ee6
jaca000 <almanso.jose@gmail.com>
2026-02-05 14:47:15 +0000

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyC3eYuvhZkhjViDLP2h8_pvhNMN_d4Q2J8",
   authDomain: "printastic-al.firebaseapp.com",
   projectId: "printastic-al",
-  storageBucket: "printastic-al.firebasestorage.app",
+  storageBucket: "printastic-al.appspot.com",
   messagingSenderId: "894753780425",
   appId: "1:894753780425:web:490b24f721c4d655fed318",
   measurementId: "G-BZBCMKQ8S7"
